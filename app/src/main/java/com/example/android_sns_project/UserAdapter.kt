5d8925152f29df0630e469a6a49f3a0332342600
ujkkk <jean207@naver.com>
2022-11-15 02:46:14 +0900

    유저창 - 사진 띄우기만 가능

--- app/src/main/java/com/example/android_sns_project/UserAdapter.kt
+++ app/src/main/java/com/example/android_sns_project/UserAdapter.kt
@@ -1,27 +1,54 @@
 package com.example.android_sns_project
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_sns_project.databinding.ContentItemBinding
 import com.example.android_sns_project.databinding.FragmentUserBinding
+import com.google.firebase.firestore.QueryDocumentSnapshot
 
-class MyViewHolder(val binding: FragmentUserBinding) : RecyclerView.ViewHolder(binding.root)
+data class Item(val id:String,val uid : String, val imagePath:String, val bmp:Bitmap){
+    constructor(doc: QueryDocumentSnapshot, bmp : Bitmap) :
+            this(doc.id,doc["uid"].toString(), doc["imagePath"].toString(), bmp)
 
-class UserAdapter (private val context: Context) : RecyclerView.Adapter<MyViewHolder>(){
+}
+class MyViewHolder(val binding: ContentItemBinding) : RecyclerView.ViewHolder(binding.root)
 
+class UserAdapter (private val context: Context, private var items: List<Item>)
+    : RecyclerView.Adapter<MyViewHolder>(){
+
+    fun interface OnItemClickListener {
+        fun onItemClick(student_id: String)
+    }
+    private var itemClickListener: OnItemClickListener? = null
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        itemClickListener = listener
+    }
+    fun updateList(newList: List<Item>) {
+        items = newList
+        notifyDataSetChanged()
+    }
+    //• RecyclerView에서 Adapter에게 필요한 만큼의 ViewHolder 생성을 요청하여 ViewHolder를 받아 두고 있음
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = FragmentUserBinding.inflate(inflater, parent, false)
+        val binding = ContentItemBinding.inflate(inflater, parent, false)
         return MyViewHolder(binding)
     }
-
+    //리스트이 항목 내용이 변경되어야 할 때 항목에 해당하는
+    //ViewHolder에 데이터를 채우도록 Adapter에게 요청
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//        val student = students[position]
-//
-//        holder.binding.textView1.text = student.id.toString()
-//        holder.binding.textView2.text = student.name
+       if(items.isEmpty()) return
+
+
+            val item = items[position]
+
+            holder.binding.imageView.setImageBitmap(item.bmp)
+
+//        holder.binding. = item.uid
+//        holder.binding.textView2.text = item.name
 //        holder.binding.root.setOnClickListener {
 //            AlertDialog.Builder(context).setMessage("You clicked ${student.name}.").show()
 //        }
@@ -33,6 +60,6 @@ class UserAdapter (private val context: Context) : RecyclerView.Adapter<MyViewHo
 
     override fun getItemCount(): Int {
       //  return students.size
-        return 1
+        return items.size
     }
 }
--- app/src/main/java/com/example/android_sns_project/UserFragment.kt
+++ app/src/main/java/com/example/android_sns_project/UserFragment.kt
@@ -1,18 +1,34 @@
 package com.example.android_sns_project
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android_sns_project.databinding.FragmentHomeBinding
+import com.example.android_sns_project.databinding.FragmentUserBinding
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 
 class UserFragment : Fragment() {
+    val db = Firebase.firestore
+    val rootRef = Firebase.storage.reference
+    private var adapter: UserAdapter? = null
+    private val itemsCollectionRef = db.collection("content")
+    private var binding: FragmentUserBinding? = null
+    var items = mutableListOf<Item>()
+    //실시간 변경 데이터 추적
+    private var snapshotListener: ListenerRegistration? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
     }
 
     override fun onCreateView(
@@ -20,6 +36,56 @@ class UserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false)
+        binding = FragmentUserBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+
+        // recyclerview setup
+        binding!!.recyclerView.layoutManager = GridLayoutManager(activity, 3)
+        adapter = activity?.let { UserAdapter(it, emptyList()) }
+        adapter?.setOnItemClickListener {
+            //updateList()
+        }
+        binding!!.accountBtnFollow.setOnClickListener{
+            updateList()
+        }
+        binding!!.recyclerView.adapter = adapter
+        updateList()
+        return binding?.root
     }
+
+    private fun updateList() {
+        //content collect에 접근
+        itemsCollectionRef.get().addOnSuccessListener {
+           // var items = mutableListOf<Item>()
+            for (doc in it) {
+                val token = doc["imagePath"].toString().split("gs://android-sns-youu.appspot.com/")
+                val ref = rootRef.child(token[1])
+
+                ref.getBytes(Long.MAX_VALUE).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result!!.size)
+                        //imgView?.setImageBitmap(bmp)
+                        items.add(Item(doc,bmp))
+
+                    }
+
+                }
+
+            }
+
+            adapter?.updateList(items)
+        }
+    }
+//    private fun getImage(path:String){
+//        val token = path.split("gs://android-sns-youu.appspot.com/")
+//        val ref = rootRef.child(token[1])
+//
+//        ref.getBytes(Long.MAX_VALUE).addOnCompleteListener {
+//            if (it.isSuccessful) {
+//                val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result!!.size)
+//                val contentImage: ImageView = customLayout.findViewById<ImageView>(R.id.contentImage)
+//                contentImage.setImageBitmap(bmp)
+//            }
+//        }
+//    }
 }
