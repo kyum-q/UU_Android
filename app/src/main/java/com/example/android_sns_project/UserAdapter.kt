9d319478d19b04fdd7db6f9f50c5dbff52f5191b
ujkkk <jean207@naver.com>
2022-11-17 02:06:04 +0900

    유저 이미지리스트 수정

--- app/src/main/java/com/example/android_sns_project/UserAdapter.kt
+++ app/src/main/java/com/example/android_sns_project/UserAdapter.kt
@@ -2,64 +2,84 @@ package com.example.android_sns_project
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_sns_project.data.Content
 import com.example.android_sns_project.databinding.ContentItemBinding
-import com.example.android_sns_project.databinding.FragmentUserBinding
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
-data class Item(val id:String,val uid : String, val imagePath:String, val bmp:Bitmap){
-    constructor(doc: QueryDocumentSnapshot, bmp : Bitmap) :
-            this(doc.id,doc["uid"].toString(), doc["imagePath"].toString(), bmp)
+data class Item(val uid : String, val imagePath:String, val bmp:Bitmap){
+    constructor(content: Content, bmp : Bitmap) :
+            this(content.uid.toString(), content.imagePath.toString(), bmp)
 
 }
 class MyViewHolder(val binding: ContentItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-class UserAdapter (private val context: Context, private var items: List<Item>)
-    : RecyclerView.Adapter<MyViewHolder>(){
-
-    fun interface OnItemClickListener {
-        fun onItemClick(student_id: String)
-    }
-    private var itemClickListener: OnItemClickListener? = null
-    fun setOnItemClickListener(listener: OnItemClickListener) {
-        itemClickListener = listener
-    }
-    fun updateList(newList: List<Item>) {
-        items = newList
-        notifyDataSetChanged()
-    }
-    //• RecyclerView에서 Adapter에게 필요한 만큼의 ViewHolder 생성을 요청하여 ViewHolder를 받아 두고 있음
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ContentItemBinding.inflate(inflater, parent, false)
-        return MyViewHolder(binding)
-    }
-    //리스트이 항목 내용이 변경되어야 할 때 항목에 해당하는
-    //ViewHolder에 데이터를 채우도록 Adapter에게 요청
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-       if(items.isEmpty()) return
-
-
-            val item = items[position]
-
-            holder.binding.imageView.setImageBitmap(item.bmp)
-
-//        holder.binding. = item.uid
-//        holder.binding.textView2.text = item.name
-//        holder.binding.root.setOnClickListener {
-//            AlertDialog.Builder(context).setMessage("You clicked ${student.name}.").show()
+class UserAdapter(private val context: Context)
+//    : RecyclerView.Adapter<MyViewHolder>(){
+//
+//    var firestore : FirebaseFirestore? =null
+//    val db = Firebase.firestore
+//    val rootRef = Firebase.storage.reference
+//
+//    var items = ArrayList<Item>()
+//    private val itemsCollectionRef = db.collection("content")
+//    init {
+//        //content collect에 접근
+//        itemsCollectionRef!!.get().addOnSuccessListener {
+//            // var items = mutableListOf<Item>()
+//            for (doc in it) {
+//                val ref = rootRef.child(doc["imagePath"].toString())
+//
+//                ref.getBytes(Long.MAX_VALUE).addOnCompleteListener {
+//                    if (it.isSuccessful) {
+//                        val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result!!.size)
+//                        //imgView?.setImageBitmap(bmp)
+//                        items.add(Item(doc, bmp))
+//
+//                    }
+//                }
+//            }
+//            notifyDataSetChanged()
+//            //adapter?.updateList(items)
 //        }
-//        holder.binding.buttonRemove.setOnClickListener {
-//            students.removeAt(holder.adapterPosition)
-//            notifyItemRemoved(holder.adapterPosition)
-//        }
-    }
-
-    override fun getItemCount(): Int {
-      //  return students.size
-        return items.size
-    }
-}
+//    }
+//    fun interface OnItemClickListener {
+//        fun onItemClick(student_id: String)
+//    }
+//    private var itemClickListener: OnItemClickListener? = null
+//    fun setOnItemClickListener(listener: OnItemClickListener) {
+//        itemClickListener = listener
+//    }
+////    fun updateList(newList: List<Item>) {
+////        items = newList
+////        notifyDataSetChanged()
+////    }
+//    //• RecyclerView에서 Adapter에게 필요한 만큼의 ViewHolder 생성을 요청하여 ViewHolder를 받아 두고 있음
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//        val inflater = LayoutInflater.from(parent.context)
+//        val binding = ContentItemBinding.inflate(inflater, parent, false)
+//        return MyViewHolder(binding)
+//    }
+//    //리스트이 항목 내용이 변경되어야 할 때 항목에 해당하는
+//    //ViewHolder에 데이터를 채우도록 Adapter에게 요청
+//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//       if(items.isEmpty()) return
+//
+//
+//            val item = items[position]
+//            holder.binding.imageView.setImageBitmap(item.bmp)
+//
+//    }
+//
+//    override fun getItemCount(): Int {
+//      //  return students.size
+//        return items.size
+//    }
+//}
--- app/src/main/java/com/example/android_sns_project/UserFragment.kt
+++ app/src/main/java/com/example/android_sns_project/UserFragment.kt
@@ -6,21 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.android_sns_project.databinding.FragmentHomeBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_sns_project.data.Content
+import com.example.android_sns_project.databinding.ContentItemBinding
 import com.example.android_sns_project.databinding.FragmentUserBinding
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.toObject
 
 
 class UserFragment : Fragment() {
     val db = Firebase.firestore
     val rootRef = Firebase.storage.reference
-    private var adapter: UserAdapter? = null
+    private var adapter: UserFragmentAdapter? = null
     private val itemsCollectionRef = db.collection("content")
     private var binding: FragmentUserBinding? = null
     var items = mutableListOf<Item>()
@@ -40,40 +43,91 @@ class UserFragment : Fragment() {
 
         // recyclerview setup
         binding!!.recyclerView.layoutManager = GridLayoutManager(activity, 3)
-        adapter = activity?.let { UserAdapter(it, emptyList()) }
-        adapter?.setOnItemClickListener {
-            //updateList()
-        }
-        binding!!.accountBtnFollow.setOnClickListener{
-            updateList()
-        }
+        adapter = UserFragmentAdapter()
+//        adapter?.setOnItemClickListener {
+//            //updateList()
+//        }
+//        binding!!.accountBtnFollow.setOnClickListener{
+//            updateList()
+//        }
         binding!!.recyclerView.adapter = adapter
-        updateList()
+       // updateList()
         return binding?.root
     }
+    inner class MyViewHolder(val binding: ContentItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-    private fun updateList() {
-        //content collect에 접근
-        itemsCollectionRef.get().addOnSuccessListener {
-           // var items = mutableListOf<Item>()
-            for (doc in it) {
-                val ref = rootRef.child( doc["imagePath"].toString())
+    inner class UserFragmentAdapter : RecyclerView.Adapter<MyViewHolder>(){
+        var contents : ArrayList<Content> = arrayListOf()
+        var items = ArrayList<Item>()
+        private val itemsCollectionRef = db.collection("content")
+        init {
+            //content collect에 접근
+            itemsCollectionRef!!.addSnapshotListener {snapshot, error ->
+                // var items = mutableListOf<Item>()
+                if(snapshot == null) return@addSnapshotListener
 
-                ref.getBytes(Long.MAX_VALUE).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result!!.size)
-                        //imgView?.setImageBitmap(bmp)
-                        items.add(Item(doc,bmp))
+                for(snapshot in snapshot.documents){
+                    contents.add(snapshot.toObject(Content::class.java)!!)
+                    for (content in contents) {
+                        val ref = rootRef.child(content.imagePath.toString())
 
+                        ref.getBytes(Long.MAX_VALUE).addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result!!.size)
+                                //imgView?.setImageBitmap(bmp)
+                                items.add(Item(content, bmp))
+
+                            }
+                        }
                     }
-
                 }
+                notifyDataSetChanged()
 
+
+                //adapter?.updateList(items)
             }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ContentItemBinding.inflate(inflater, parent, false)
+            var width = resources.displayMetrics.widthPixels/3
+            binding.imageView.layoutParams = LinearLayoutCompat.LayoutParams(width,width)
+            return MyViewHolder(binding)
+        }
 
-            adapter?.updateList(items)
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            if(items.isEmpty()) return
+
+
+            val item = items[position]
+            holder.binding.imageView.setImageBitmap(item.bmp)
+        }
+
+        override fun getItemCount(): Int {
+            return items.size
         }
     }
+    private fun updateList() {
+//        //content collect에 접근
+//        itemsCollectionRef.get().addOnSuccessListener {
+//           // var items = mutableListOf<Item>()
+//            for (doc in it) {
+//                val ref = rootRef.child(doc["imagePath"].toString())
+//
+//                ref.getBytes(Long.MAX_VALUE).addOnCompleteListener {
+//                    if (it.isSuccessful) {
+//                        val bmp = BitmapFactory.decodeByteArray(it.result, 0, it.result!!.size)
+//                        //imgView?.setImageBitmap(bmp)
+//                        items.add(Item(doc, bmp))
+//
+//                    }
+//                }
+//            }
+//            adapter?.updateList(items)
+//        }
+    }
+
+
 //    private fun getImage(path:String){
 //        val token = path.split("gs://android-sns-youu.appspot.com/")
 //        val ref = rootRef.child(token[1])
