843d43eb2db2ee5913a68f821e6758c06a55b1fc
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-11-24 01:40:59 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/android_sns_project/MainActivity.kt
+++ app/src/main/java/com/example/android_sns_project/MainActivity.kt
@@ -61,23 +61,7 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupActionBarWithNavController(this, nhf.navController, appBarConfig)
 
         getFCMToken()
-
-        val TAG: String = "안녕"
-
-            //메인 액티비티에서 등록 토큰 가져오기
-            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                    return@OnCompleteListener
-                }
-
-                // Get new FCM registration token
-                val token = task.result
-
-                // Log and toast
-                val msg = "test"
-                Log.d(TAG, msg)
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        
             })
         }
     private fun getFCMToken(): String?{
